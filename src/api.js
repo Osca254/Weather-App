256eb4ef892155a8cba291f951b395ca8612bd16
moringa <oscarokumu47@gmail.com>
2022-12-23 21:16:16 +0300

    created a file to store the api

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2778a49beemsh06512f95a9e40edp154245jsn550235e13880',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '591f8c39ecdaeffbe9ec933122945893';
+
